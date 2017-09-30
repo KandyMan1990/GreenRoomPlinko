b2761061ae8ee670543e8f33e069fb4c3bd30e09
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2017-09-30 13:49:05 +0100

    Organised project better, added platforms

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    [SerializeField]
+    int direction = 1;
+    [SerializeField]
+    float speed = 2f;
+	
+	void Update () {
+        transform.position = new Vector3(transform.position.x + (direction * speed * Time.deltaTime), transform.position.y, transform.position.z);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+            direction *= -1;
+    }
+}
