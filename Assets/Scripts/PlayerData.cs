5b93b583ab43d7b7fa6407fe95464d272d2d14c9
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2017-10-02 22:46:20 +0100

    Added player data class, did some testing with the add new player button

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿[System.Serializable]
+public class PlayerData
+{
+    public string Name;
+    public int Wins;
+
+    public PlayerData(string name)
+    {
+        Name = name;
+        Wins = 0;
+    }
+
+    public PlayerData(string name, int wins)
+    {
+        Name = name;
+        Wins = wins;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,23 +8,47 @@ public class UIManager : MonoBehaviour
     GameObject PlayerUIPrefab;
     [SerializeField]
     Transform Panel;
-    List<GameObject> Players = new List<GameObject>();
+    [SerializeField]
+    Button NewPlayerButton;
+    List<PlayerData> Players = new List<PlayerData>();
 
     void Awake()
     {
-        //read in all player data (players, wins) into Players.  Type will have to change from gameobject to player to store name and wins
+        //read in all player data (players, wins) into Players
+        //testing stuff below
+        Players.Add(new PlayerData("Jim"));
+        Players.Add(new PlayerData("Jam", 3));
+        Players.Add(new PlayerData("Dude", 5));
     }
 
     void Start()
     {
-        foreach (GameObject player in Players)
+        foreach (PlayerData player in Players)
         {
-            Instantiate(player, Panel);
+            InstantiateNewPlayer(player);
         }
     }
 
     void OnApplicationQuit()
     {
         //write all player data from players to file
+    }
+
+    public void NewPlayerButtonOnClick()
+    {
+        //Show popup
+        //popup allows name to be entered
+        //add name to list of players
+        //instantiate the new player into the panel
+        //testing stuff below
+        InstantiateNewPlayer(new PlayerData("Danielle", 10));
+    }
+
+    void InstantiateNewPlayer(PlayerData player)
+    {
+        GameObject go = Instantiate(PlayerUIPrefab, Panel);
+        Text[] texts = go.GetComponentsInChildren<Text>();
+        texts[0].text = player.Name;
+        texts[1].text = player.Wins.ToString();
     }
 }
