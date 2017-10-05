9e7d84608718daa11d2c77d1ed70d039fef854de
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2017-10-05 22:19:17 +0100

    Started work on the win/lose cells

---
+++ Assets/Scripts/LandingZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class LandingZone : MonoBehaviour
+{
+    [SerializeField]
+    Color WinColour;
+    [SerializeField]
+    Color LoseColour;
+    [SerializeField]
+    Color InstantWinColour;
+    [SerializeField]
+    bool winZone;
+
+    static bool instantWin;
+
+    SpriteRenderer Renderer;
+    BoxCollider2D Collider;
+
+    void Awake()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        Collider = GetComponent<BoxCollider2D>();
+
+        if (winZone && !instantWin && Random.Range(0, 100) <= 1)
+        {
+            instantWin = true;
+            Renderer.color = InstantWinColour;
+            return;
+        }
+
+        Renderer.color = winZone ? WinColour : LoseColour;
+    }
+}
