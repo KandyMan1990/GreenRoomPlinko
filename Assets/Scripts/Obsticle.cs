14597a809a21aa8c14cac00494e2dfd45984904f
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2017-11-03 21:40:46 +0000

    Stop players from getting stuck

---
+++ Assets/Scripts/Obsticle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Obsticle : MonoBehaviour
+{
+    CircleCollider2D circleCollider;
+
+    void Awake()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(CheckCollision(col.gameObject));
+        }
+    }
+
+    IEnumerator CheckCollision(GameObject go)
+    {
+        Vector3 currentPos = go.transform.position;
+
+        yield return new WaitForSeconds(1f);
+
+        if (go.transform.position == currentPos)
+        {
+            circleCollider.enabled = false;
+            yield return new WaitForSeconds(1f);
+            circleCollider.enabled = true;
+        }
+    }
+}
