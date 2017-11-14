72c7604822b566a4c4d7c1a42b5d03d02cf78e0a
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2017-11-14 21:06:31 +0000

    Frame rate halved when not needed.  Relates to issue #9

---
+++ Assets/Scripts/SetTargetFPS.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SetTargetFPS : MonoBehaviour
+{
+    [SerializeField]
+    bool halfFrameRate;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = halfFrameRate ? 2 : 1;
+    }
+}
