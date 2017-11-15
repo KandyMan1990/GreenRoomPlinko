c09260abc824e68235ab670c0bc99b8d05fed7e2
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2017-11-15 22:32:02 +0000

    Loading scenes should be a bit quicker.  Release of v1.0.7

---
+++ Assets/Scripts/LoadSceneImmediately.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneImmediately : MonoBehaviour
+{
+    AsyncOperation async;
+
+    // Use this for initialization
+    void Start()
+    {
+        async = SceneManager.LoadSceneAsync("Main");
+        async.allowSceneActivation = false;
+    }
+
+    public void LoadMain()
+    {
+        async.allowSceneActivation = true;
+    }
+}
