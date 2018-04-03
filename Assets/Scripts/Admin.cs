74f304fd8fda91cd7f1f9b24d0ea5575824ff2eb
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2018-04-03 20:46:15 +0100

    Started on Admin Mode
    Can enable/disable Admin Mode via password

---
+++ Assets/Scripts/Admin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Admin : MonoBehaviour
+{
+    public static Admin Instance { get; private set; }
+
+    const string _password = "C0FF33!!";
+    bool _active = false;
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public bool isActive
+    {
+        get { return _active; }
+    }
+
+    public void Enable(string password)
+    {
+        _active = password == _password;
+    }
+
+
+}
---
+++ Assets/Scripts/AdminButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdminButton : MonoBehaviour
+{
+    string enable = "Enable Admin";
+    string disable = "Disable Admin";
+
+    void OnEnable()
+    {
+        GetComponent<Text>().text = Admin.Instance.isActive ? disable : enable;
+    }
+}
