f03bcdb0c9a78725a77016c8e4b1b8ec6251dcc3
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2018-06-07 22:06:41 +0100

    Created basic login/registration scene

---
+++ Assets/Scripts/GameSparks/LoginPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using GameSparks.Api.Requests;
+using UnityEngine.SceneManagement;
+
+public class LoginPlayer : MonoBehaviour
+{
+    [SerializeField] Text userName;
+    [SerializeField] InputField password;
+
+    public void Login()
+    {
+        Debug.Log("Logging In");
+
+        if (userName.text == string.Empty || password.text == string.Empty)
+        {
+            Debug.LogError("Empty username or password fields");
+            return;
+        }
+
+        new AuthenticationRequest().SetUserName(userName.text).SetPassword(password.text).Send((authResponse) => {
+            if (!authResponse.HasErrors)
+            {
+                Debug.Log(string.Format("{0} Authenticated", userName.text));
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                Debug.LogError("Error Authenticating Player");
+                Debug.LogError(authResponse.Errors.JSON);
+            }
+        });
+    }
+}
---
+++ Assets/Scripts/GameSparks/RegisterPlayer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using GameSparks.Api.Requests;
+using UnityEngine.SceneManagement;
+
+public class RegisterPlayer : MonoBehaviour
+{
+    [SerializeField] Text userName, password, repeatPassword;
+
+    public void Register()
+    {
+        Debug.Log("Registering");
+
+        if (userName.text == string.Empty || password.text == string.Empty || repeatPassword.text == string.Empty)
+        {
+            Debug.LogError("Empty username or password fields");
+            return;
+        }
+
+        if (password.text != repeatPassword.text)
+        {
+            Debug.LogError("Password does not match repeat password");
+            return;
+        }
+
+        new RegistrationRequest()
+            .SetDisplayName(userName.text)
+            .SetPassword(password.text)
+            .SetUserName(userName.text)
+            .Send((registrationResponse) => {
+                if (!registrationResponse.HasErrors)
+                {
+                    Debug.Log(string.Format("{0} Registered", userName.text));
+                    Debug.Log(string.Format("Authenticating {0}", userName.text));
+
+                    new AuthenticationRequest().SetUserName(userName.text).SetPassword(password.text).Send((authResponse) => {
+                        if (!authResponse.HasErrors)
+                        {
+                            Debug.Log(string.Format("{0} Authenticated", userName.text));
+                            SceneManager.LoadScene(1);
+                        }
+                        else
+                        {
+                            Debug.LogError("Error Authenticating Player");
+                            Debug.LogError(authResponse.Errors);
+                        }
+                    });
+                }
+                else
+                {
+                    Debug.LogError("Error Registering Player");
+                    Debug.LogError(registrationResponse.Errors);
+                }
+            });
+    }
+}
