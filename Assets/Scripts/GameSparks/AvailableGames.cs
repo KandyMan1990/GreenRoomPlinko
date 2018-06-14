31c2aa2bff7c299258841ad6bb6e669e88f3b156
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2018-06-14 22:19:43 +0100

    Lobby styling, made prefabs ro show available matches, attempting to create a match and show it in the lobby as joinable

---
+++ Assets/Scripts/GameSparks/AvailableGames.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+public class AvailableGames : MonoBehaviour
+{
+    [SerializeField]
+    GameObject availableMatchPrefab;
+    WaitForSeconds wait;
+    List<string> shortCodes;
+
+    // Use this for initialization
+    void Start()
+    {
+        wait = new WaitForSeconds(5f);
+        shortCodes = new List<string>();
+        shortCodes.Add("PLINKO");
+
+        StartCoroutine(PollForMatches());
+    }
+
+    IEnumerator PollForMatches()
+    {
+        while (true)
+        {
+            new FindChallengeRequest()
+                .SetAccessType("PUBLIC")
+                .SetShortCode(shortCodes)
+                .Send(response =>
+                {
+                    Debug.Log(response);
+                });
+
+            yield return wait;
+        }
+    }
+}
---
+++ Assets/Scripts/GameSparks/CreateGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+public class CreateGame : MonoBehaviour
+{
+    System.DateTime dateTime;
+
+    public void OnClick()
+    {
+        dateTime = System.DateTime.Now;
+        dateTime = dateTime.AddMinutes(5);
+
+        new CreateChallengeRequest()
+            .SetAccessType("PUBLIC")
+            .SetAutoStartJoinedChallengeOnMaxPlayers(false)
+            .SetChallengeMessage("I challenge you to a plinko!")
+            .SetChallengeShortCode("PLINKO")
+            .SetEndTime(dateTime)
+            .SetMaxPlayers(10)
+            .SetMinPlayers(2)
+            .Send(response =>
+            {
+                Debug.Log(response); // available games panel should check periodically for challenges
+            });
+    }
+}
