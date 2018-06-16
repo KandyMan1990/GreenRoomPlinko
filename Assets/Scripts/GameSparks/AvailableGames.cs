1e349a8d02e524d4e320c14fd024ac5ed7972e3a
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2018-06-16 18:38:34 +0100

    Creating game takes you to main scene.  Can now see list of available games, clicking one takes you to main scene

--- Assets/Scripts/GameSparks/AvailableGames.cs
+++ Assets/Scripts/GameSparks/AvailableGames.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using GameSparks.Api;
 using GameSparks.Api.Requests;
 using GameSparks.Api.Responses;
+using GameSparks.Core;
 
 public class AvailableGames : MonoBehaviour
 {
-    [SerializeField]
-    GameObject availableMatchPrefab;
+    [SerializeField] GameObject availableMatchPrefab;
+    [SerializeField] Transform availableGamesPanel;
     WaitForSeconds wait;
     List<string> shortCodes;
 
@@ -18,6 +21,12 @@ public class AvailableGames : MonoBehaviour
         wait = new WaitForSeconds(5f);
         shortCodes = new List<string>();
         shortCodes.Add("PLINKO");
+
+        for (int i = 0; i < 7; i++)
+        {
+            GameObject go = Instantiate(availableMatchPrefab, availableGamesPanel);
+            go.SetActive(false);
+        }
 
         StartCoroutine(PollForMatches());
     }
@@ -29,9 +38,32 @@ public class AvailableGames : MonoBehaviour
             new FindChallengeRequest()
                 .SetAccessType("PUBLIC")
                 .SetShortCode(shortCodes)
-                .Send(response =>
+                .Send((requestResponse) =>
                 {
-                    Debug.Log(response);
+                    GSEnumerable<FindChallengeResponse._Challenge> challengeInstances = requestResponse.ChallengeInstances;
+
+                    foreach (Transform child in availableGamesPanel)
+                    {
+                        child.GetComponentInChildren<Button>().onClick.RemoveAllListeners();
+                        child.gameObject.SetActive(false);
+                    }
+
+                    int i = 0;
+                    foreach (FindChallengeResponse._Challenge item in challengeInstances)
+                    {
+                        GameObject go = availableGamesPanel.GetChild(i).gameObject;
+                        go.GetComponentInChildren<Text>().text = item.ChallengeMessage;
+                        go.GetComponentInChildren<Button>().onClick.AddListener(() =>
+                        {
+                            new AcceptChallengeRequest()
+                                .SetChallengeInstanceId(item.ChallengeId)
+                                .SetMessage(string.Empty)
+                                .Send((response) => {
+                                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                                });
+                        });
+                        go.SetActive(true);
+                    }
                 });
 
             yield return wait;
--- Assets/Scripts/GameSparks/CreateGame.cs
+++ Assets/Scripts/GameSparks/CreateGame.cs
@@ -1,28 +1,52 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using GameSparks.Api;
 using GameSparks.Api.Requests;
 using GameSparks.Api.Responses;
+using GameSparks.Core;
 
 public class CreateGame : MonoBehaviour
 {
-    System.DateTime dateTime;
+    DateTime dateTime;
+    GSData loadData;
+    AccountDetailsResponse accountData;
+    string room;
 
     public void OnClick()
     {
-        dateTime = System.DateTime.Now;
-        dateTime = dateTime.AddMinutes(5);
+        new AccountDetailsRequest().Send((accountResponse) =>
+        {
+            accountData = accountResponse;
 
-        new CreateChallengeRequest()
-            .SetAccessType("PUBLIC")
-            .SetAutoStartJoinedChallengeOnMaxPlayers(false)
-            .SetChallengeMessage("I challenge you to a plinko!")
-            .SetChallengeShortCode("PLINKO")
-            .SetEndTime(dateTime)
-            .SetMaxPlayers(10)
-            .SetMinPlayers(2)
-            .Send(response =>
+            new LogEventRequest().SetEventKey("LOAD_PLAYER").Send((loadResponse) =>
             {
-                Debug.Log(response); // available games panel should check periodically for challenges
+                if (!loadResponse.HasErrors)
+                {
+                    dateTime = DateTime.Now;
+                    dateTime = dateTime.AddMinutes(5);
+
+                    loadData = loadResponse.ScriptData.GetGSData("playerData");
+
+                    room = loadData.GetString("room");
+                    Char C = Char.ToUpper(room[0]);
+                    room = room.Replace(room[0], C);
+                    room += " Room";
+
+                    new CreateChallengeRequest()
+                        .SetAccessType("PUBLIC")
+                        .SetAutoStartJoinedChallengeOnMaxPlayers(false)
+                        .SetChallengeMessage(room)
+                        .SetChallengeShortCode("PLINKO")
+                        .SetEndTime(dateTime)
+                        .SetMaxPlayers(10)
+                        .SetMinPlayers(2)
+                        .Send((response) =>
+                        {
+                            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                        });
+                }
             });
+        });
     }
 }
